1a474316755c909b63f897d21291ea1843107193
Kayden1268 <kaydenneo10@gmail.com>
2023-08-08 09:14:03 +0800

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [card, setCard] = useState([])
 
+
+    const getData = async () => {
+        await fetch("https://co8xhcw1m7.execute-api.us-east-1.amazonaws.com/default/getItems")
+        .then((response) => response.json())
+        .then((data) => {
+            
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    useEffect(() => {
+        getData()
+    }, []);
     
 
 
